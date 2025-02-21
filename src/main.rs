c0d0f1fd77341444b169b0670a8ac3f23b9ac281
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-02-21 15:52:54 +0100

    feat: In Memory

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,27 @@ use std::{f32, io};
 use anyhow::anyhow;
 use fst::{automaton::Levenshtein, IntoStreamer, Map, MapBuilder, Streamer};
 use levenshtein::levenshtein;
-use memmap::Mmap;
-use rocksdb::DB;
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 struct GeoNamesData {
     name: String,
     latitude: f32,
     longitude: f32,
+    feature_class: String,
     feature_code: String,
     country_code: String,
 }
 
 fn main() -> anyhow::Result<()> {
     let mut search_terms: Vec<(String, u64)> = Vec::new();
-    let db = DB::open_default("_rocksdb").unwrap();
+    let mut data_store: HashMap<u64, GeoNamesData> = HashMap::new();
 
-    parse_geonames_file("data/geonames/cities500.txt", &mut search_terms, &db)?;
-    parse_geonames_file("data/geonames/DE.txt", &mut search_terms, &db)?;
+    parse_geonames_file(
+        "data/geonames/DE.txt",
+        // "data/geonames/allCountries.txt",
+        &mut search_terms,
+        &mut data_store,
+    )?;
 
     println!("Read {} search terms", search_terms.len());
 
@@ -34,42 +37,40 @@ fn main() -> anyhow::Result<()> {
         search_terms.len()
     );
 
-    // This is where we'll write our map to.
-    let mut wtr = io::BufWriter::new(File::create("map.fst")?);
-
-    // Create a builder that can be used to insert new key-value pairs.
-    let mut build = MapBuilder::new(wtr)?;
+    let mut build = MapBuilder::memory();
     for (term, geoname_id) in search_terms {
         build.insert(term, geoname_id)?;
     }
+    let bytes = build.into_inner()?;
+    let num_bytes = bytes.len();
+    let map = Map::new(bytes)?;
 
-    // Finish construction of the map and flush its contents to disk.
-    build.finish()?;
-
-    println!("Wrote map to disk");
-
-    let mmap = unsafe { Mmap::map(&File::open("map.fst")?)? };
-    let map = Map::new(mmap)?;
-
-    println!("Read map from disk");
+    println!("Built FST with {} bytes", num_bytes);
 
     let mut buffer = String::new();
     loop {
         io::stdin().read_line(&mut buffer)?;
 
-        let max_distance: u32 = (buffer.trim().len() / 8).try_into().unwrap_or(4u32);
+        let max_distance: u32 = (buffer.trim().len() / 4).try_into().unwrap_or(4u32);
         let query = Levenshtein::new(&buffer.trim(), max_distance)
             .or_else(|_| Levenshtein::new(&buffer, 1))?;
         let mut stream = map.search_with_state(&query).into_stream();
 
-        while let Some((key, val, size)) = stream.next() {
+        let mut results = Vec::new();
+        while let Some((key, val, _)) = stream.next() {
             let key = String::from_utf8_lossy(key).to_string();
 
-            let val = db.get(val.to_be_bytes())?.unwrap();
-            let val: GeoNamesData = bincode::deserialize(&val).unwrap();
-
             let dist = levenshtein(&buffer.trim(), &key);
-            println!("key: {key:?}, size: {size:?}, dist: {dist:?}, val: {val:?}");
+
+            let val = data_store.get(&val).unwrap();
+
+            results.push((key, dist, val));
+        }
+
+        // sort by dist
+        results.sort_by(|a, b| a.1.cmp(&b.1));
+        for result in results {
+            println!("{} dist:{} {:?}", result.0, result.1, result.2);
         }
         println!();
         buffer.clear();
@@ -81,34 +82,36 @@ fn main() -> anyhow::Result<()> {
 fn parse_geonames_file(
     path: &str,
     search_terms: &mut Vec<(String, u64)>,
-    db: &DB,
+    data_store: &mut HashMap<u64, GeoNamesData>,
 ) -> Result<(), anyhow::Error> {
     let mut rdr = csv::ReaderBuilder::new()
         .delimiter(b'\t')
         .from_reader(io::BufReader::new(File::open(path)?));
+
     for row in rdr.records() {
         let record = row?;
 
         let geoname_id: u64 = record.get(0).ok_or(anyhow!("no geoname_id"))?.parse()?;
-        let name: &str = record.get(1).ok_or(anyhow!("no name"))?;
+        let name: String = record.get(1).ok_or(anyhow!("no name"))?.to_string();
 
         let latitude: f32 = parse_float_else_nan(record.get(4));
         let longitude: f32 = parse_float_else_nan(record.get(5));
-        let feature_code: &str = record.get(7).unwrap_or("<missing:feature_code>");
-        let country_code: &str = record.get(8).unwrap_or("<missing:country_code>");
+        let feature_class: String = record.get(6).unwrap_or("<missing>").to_string();
+        let feature_code: String = record.get(7).unwrap_or("<missing>").to_string();
+        let country_code: String = record.get(8).unwrap_or("<missing>").to_string();
 
         let data = GeoNamesData {
-            name: name.to_string(),
+            name: name.clone(),
             latitude,
             longitude,
-            feature_code: feature_code.to_string(),
-            country_code: country_code.to_string(),
+            feature_class,
+            feature_code,
+            country_code,
         };
 
-        db.put(geoname_id.to_be_bytes(), bincode::serialize(&data)?)
-            .unwrap();
+        data_store.insert(geoname_id, data);
 
-        search_terms.push((name.to_string(), geoname_id));
+        search_terms.push((name, geoname_id));
         if let Some(alternate_names) = record.get(3) {
             for name in alternate_names.split(',') {
                 if name.trim().is_empty() {
